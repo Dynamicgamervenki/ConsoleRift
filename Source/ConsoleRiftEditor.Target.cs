96e3092bf049bbb2f52525f127ba152c5c474056
Venki <18534venkanna@gmail.com>
2025-01-08 12:33:41 +0530

    Project Version Changed To UnrealEngine 5.4

--- Source/ConsoleRiftEditor.Target.cs
+++ Source/ConsoleRiftEditor.Target.cs
@@ -5,11 +5,20 @@ using System.Collections.Generic;
 
 public class ConsoleRiftEditorTarget : TargetRules
 {
-	public ConsoleRiftEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+    public ConsoleRiftEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
 
-		ExtraModuleNames.AddRange( new string[] { "ConsoleRift" } );
-	}
+        // Allow overriding build environment
+        bOverrideBuildEnvironment = true;
+
+        // Suppress backward-compatible build warnings
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+
+        // Ensure the latest include order is used
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+        ExtraModuleNames.AddRange(new string[] { "ConsoleRift" });
+    }
 }
